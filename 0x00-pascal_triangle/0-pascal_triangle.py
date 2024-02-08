c5af0cf5164412e8635e2c13131cb2c9ce9523e6
wambui-g <grayciekamau03@gmail.com>
2024-02-08 08:02:47 +0300

    all

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to return pascal triangle"""
+
+
+def pascal_triangle(n):
+    """pascal triangle function"""
+    lists = []
+    if n == 0:
+        return lists
+    for i in range(n):
+        lists.append([])
+        lists[i].append(1)
+        if (i > 0):
+            for j in range(1, i):
+                lists[i].append(lists[i - 1][j - 1] + lists[i - 1][j])
+            lists[i].append(1)
+    return lists
