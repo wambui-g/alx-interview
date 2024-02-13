11dbb620d606f954aa7595303dde34e0afcc1df8
wambui-g <grayciekamau03@gmail.com>
2024-02-13 08:50:43 +0300

    all

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function to determine if boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """function that takes list of lists"""
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
